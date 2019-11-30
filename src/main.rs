745c8e72efb84259cdff09b5424759c4866fd88f
modeverv <modeverv@gmail.com>
2019-11-30 10:01:19 +0900

    2019-11-30 10:01:19

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::{Write,BufRead, BufReader, Error, ErrorKind};
+
+
 fn main() {
     println!("Hello, world!");
     let i:i32 = r();
     println!("{}",i);
+    let result = hello_to_file("/Users/seijiro/Downloads/a.txt");
 }
 fn r<T: std::str::FromStr>() -> T {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).ok();
     s.trim().parse().ok().unwrap()
+}
+
+fn hello_to_file(path: impl AsRef<Path>) -> Result<(),Error> {
+    let mut file = File::create(path.as_ref())?;
+    write!(file,"Hello,File")?;
+    Ok(())
 }
